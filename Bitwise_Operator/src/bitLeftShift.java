78ce466ba13d9b55081589ede4ce6148f46c30b0
Dikshant Koriwar <dikshantkoriwar29@gmail.com>
2023-12-10 12:03:31 +0530

    first commit

---
+++ Bitwise_Operator/src/bitLeftShift.java
@@ -0,0 +1,10 @@
+public class bitLeftShift {
+    public static void main(String[] args) {
+        int a=2;
+        for(int i=0;i<2;i++){
+            a=a<<1;
+        }
+        System.out.println(a);
+    }
+    
+}
