03ef9cd5796ec33acae72e809a65858f6e56771d
Dikshant Koriwar <dikshantkoriwar29@gmail.com>
2023-12-21 08:46:33 +0530

    intail commit

---
+++ Bitwise_Operator/src/RepresentBinary.java
@@ -0,0 +1,17 @@
+public class RepresentBinary {
+    public static void main(String[] args) {
+        int n=45;
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(setBits(n));
+    }
+    private static int setBits(int n){
+        int count =0;
+        while(n >0){
+            count++;
+            n-=(n & -n);
+        }
+        
+        return count;
+    }
+    
+}
